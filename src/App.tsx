2dab9b32d954a67f1f76d854035cbb774f250c08
Daniel Scott <dasc0325@colorado.edu>
2019-11-19 00:42:59 -0700

    adding AWS Amplify
     - removing bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { HashRouter as Router } from 'react-router-dom'
 import { Routes } from './Routes'
-import { Navbar } from "react-bootstrap";
 import React from 'react'
 
 const App: React.FC = () => {
---
+++ src/config.tsx
@@ -0,0 +1,12 @@
+export default {
+	apiGateway: {
+		REGION: 'us-east-2',
+		URL: 'https://6mmp4d6ot3.execute-api.us-east-2.amazonaws.com/prod',
+	},
+	cognito: {
+		REGION: 'us-east-2',
+		USER_POOL_ID: 'us-east-2_SjodzWhX3',
+		APP_CLIENT_ID: '2b3hgbui2df777evde6e23l9ru',
+		IDENTITY_POOL_ID: 'us-east-2:3f16a5b5-c077-4d7d-bd91-40efabb0b114',
+	},
+}
