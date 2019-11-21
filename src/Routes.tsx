ca3b799eef51fbc66e1b16632960042833687ee0
Daniel Scott <dasc0325@colorado.edu>
2019-11-20 23:14:34 -0700

    set up auth on dashboard
     - gutted Dashboard

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Home } from 'pages/Home'
 import { Login } from 'common/login'
-import { Dashboard } from 'pages/Dashboard'
+import Dashboard from 'pages/Dashboard'
 
 export const Routes = () => {
 	return (
 		<Switch>
 			<Route exact path="/" component={Home} />
 			<Route exact path="/login" component={Login} />
-			<Route exact path="/dashboard/:username" component={Dashboard} />
+			<Route exact path="/dashboard" component={Dashboard} />
 		</Switch>
 	)
 }
