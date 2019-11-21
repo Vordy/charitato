fd9b28829383df831262d1d8382ec88d72f0b9db
Daniel Scott <dasc0325@colorado.edu>
2019-11-20 23:37:57 -0700

    enabled link to dashboard

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,72 +6,75 @@ import { Colors } from 'theme/Colors'
 import { Button } from 'common/button/Button'
 import { ButtonSizes, ButtonTypes } from 'common/button/ButtonUtils'
 import { Homeblob } from 'assets/blobs/Homeblob'
+import { useHistory } from 'react-router'
 
 const HomeContainer = styled.div`
-	width: 100%;
-	height: 100%;
-	flex-direction: column;
-	justify-content: center;
-	align-items: flex-start;
+    width: 100%;
+    height: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
 
-	display: flex;
-	color: ${Colors.RichDark};
-	background-color: ${Colors.White};
-	font-family: 'Helvetica Nueue', roboto, Arial, Helvetica, sans-serif;
+    display: flex;
+    color: ${Colors.RichDark};
+    background-color: ${Colors.White};
+    font-family: 'Helvetica Nueue', roboto, Arial, Helvetica, sans-serif;
 `
 
 const SplashHeader = styled.div`
-	font-size: 5em;
-	font-weight: 700;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	display: flex;
-	padding-left: 32px;
-	padding-top: 100px;
+    font-size: 5em;
+    font-weight: 700;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    padding-left: 32px;
+    padding-top: 100px;
 `
 
 const BoldText = styled.div`
-	font-family: 'Helvetica Nueue', roboto, Arial, Helvetica, sans-serif;
-	font-style: normal;
-	font-weight: bold;
-	font-size: 56px;
-	line-height: 34px;
-	text-align: center;
-	padding-bottom: 16px;
+    font-family: 'Helvetica Nueue', roboto, Arial, Helvetica, sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 56px;
+    line-height: 34px;
+    text-align: center;
+    padding-bottom: 16px;
 `
 
 const SubText = styled.div`
-	font-family: 'Helvetica Nueue', roboto, Arial, Helvetica, sans-serif;
-	font-style: normal;
-	font-weight: 100;
-	font-size: 56px;
-	line-height: 84px;
+    font-family: 'Helvetica Nueue', roboto, Arial, Helvetica, sans-serif;
+    font-style: normal;
+    font-weight: 100;
+    font-size: 56px;
+    line-height: 84px;
 `
 
 export const Home = () => {
-	return (
-		<>
-			<HomeContainer>
-				<SplashHeader>
-					<BoldText>
-						Let’s make the world better <br />
-						<SubText>one potato at a time</SubText>
-					</BoldText>
+    const history = useHistory()
 
-					<Button
-						buttonType={ButtonTypes.Primary}
-						buttonSize={ButtonSizes.Large}
-						text={'Send a potato'}
-						onClickHandler={(e: React.MouseEvent) => {
-							console.log('boom')
-						}}
-					/>
-				</SplashHeader>
-			</HomeContainer>
-			<Homeblob
-				style={{ position: 'absolute', top: '-10px', right: '-10px' }}
-			/>
-		</>
-	)
+    return (
+        <>
+            <HomeContainer>
+                <SplashHeader>
+                    <BoldText>
+                        Let’s make the world better <br />
+                        <SubText>one potato at a time</SubText>
+                    </BoldText>
+
+                    <Button
+                        buttonType={ButtonTypes.Primary}
+                        buttonSize={ButtonSizes.Large}
+                        text={'Send a potato'}
+                        onClickHandler={(e: React.MouseEvent) => {
+                            history.push('/dashboard')
+                        }}
+                    />
+                </SplashHeader>
+            </HomeContainer>
+            <Homeblob
+                style={{ position: 'absolute', top: '-10px', right: '-10px' }}
+            />
+        </>
+    )
 }
