438eea0d63dd6b3c69a0b4091479f9208b020474
Daniel Scott <dasc0325@colorado.edu>
2019-11-24 22:38:58 -0700

    sort imports - App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router } from 'react-router-dom'
+import { NavBar } from './common/navbar'
 import { Routes } from './Routes'
 import Amplify from 'aws-amplify'
 import awsconfig from './aws-exports'
 import React from 'react'
-import { NavBar } from './common/navbar'
 
 Amplify.configure(awsconfig)
 
