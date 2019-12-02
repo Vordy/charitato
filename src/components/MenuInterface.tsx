c76511c04b1acfc9af0885517786de0c92af3185
gordongu <1559005+gordongu@users.noreply.github.com>
2019-12-02 01:03:46 -0700

    Added menu interface file

---
+++ src/components/MenuInterface.tsx
@@ -0,0 +1,11 @@
+import { MenuContainer, MenuItem, MenuItemSelected } from "./Interface";
+import React from 'react'
+
+export const MilestonesInterface = () => {
+    return (
+        <MenuContainer>
+            <MenuItem></MenuItem>
+            <MenuItemSelected></MenuItemSelected>
+        </MenuContainer>
+    );
+}
