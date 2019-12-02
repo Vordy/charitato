0e8f53640ef2229320aa84a3dbe30d02d9e07ee0
gordongu <1559005+gordongu@users.noreply.github.com>
2019-12-02 01:08:33 -0700

    Fixed interface name

--- src/components/MenuInterface.tsx
+++ src/components/MenuInterface.tsx
@@ -1,7 +1,7 @@
 import { MenuContainer, MenuItem, MenuItemSelected } from "./Interface";
 import React from 'react'
 
-export const MilestonesInterface = () => {
+export const MenuInterface = () => {
     return (
         <MenuContainer>
             <MenuItem></MenuItem>
