b10cb088ff63921f4759ec08abf270efbe1eb1dc
Daniel Scott <dasc0325@colorado.edu>
2019-12-02 22:20:16 -0700

    minor boilerplate code for PotatoInterface

--- src/components/PotatoInterface.tsx
+++ src/components/PotatoInterface.tsx
@@ -1,13 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { UserContext } from 'pages/Dashboard'
 
 export const PotatoInterface = () => {
     const user = useContext(UserContext)
+    const [hasPotato, setHasPotato] = useState(false)
+
+    useEffect(() => {
+        if (user.state.instance) {
+            setHasPotato(user.state.instance.hasPotato)
+        }
+    }, [user.state.instance])
 
     return (
         <div>
             <div>Potato Interface</div>
             <div>Username: {user.state.username}</div>
+            {hasPotato && <div>This user has a potato!</div>}
+            {!hasPotato && <div>This user does not have a potato!</div>}
         </div>
     )
 }
