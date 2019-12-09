8da57ff0e3e1f6bca3ac1630ecce96288e02460f
Daniel Scott <dasc0325@colorado.edu>
2019-12-09 14:34:51 -0700

    [doc] added commenting to styles

--- src/common/dashboard/potato_styles.tsx
+++ src/common/dashboard/potato_styles.tsx
@@ -9,21 +9,25 @@ export const InterfaceContainer = styled.div`
     background-color: ${getTheme().background};
     color: ${getTheme().foreground};
 
+    /* Font */
     font-family: 'Helvetica Nueue', roboto, Arial, Helvetica, sans-serif;
     font-size: 0.9vw;
+    text-align: center;
+
+    /* Size */
     height: 100%;
     width: 100%;
-    text-align: center;
 `
 
 export const PotatoModeContainer = styled.div`
-    display: grid;
+    /* Size */
     height: 100%;
     width: 100%;
 
+    /* Layout */
+    display: grid;
     grid-template-columns: 2fr 1fr 1fr 2fr;
     grid-template-rows: 20% 30% 10% 10% 10% 20%;
-
     grid-template-areas:
         '. . . .'
         '. splash splash .'
@@ -31,38 +35,54 @@ export const PotatoModeContainer = styled.div`
         '. subtext subtext .'
         '. button1 button2 .'
         '. . . .';
-
     place-items: center center;
 `
 
 export const PotatoTitleText = styled.div`
-    font-weight: bold;
+    /* Layout */
     grid-area: text;
+
+    /* Font */
+    font-weight: bold;
     font-size: 2.5vw;
     font-weight: bold;
 `
 
 export const PotatoSubTitleText = styled.div`
-    margin: 0px;
+    /* Layout */
     grid-area: subtext;
+    margin: 0px;
+
+    /* Font */
     font-size: 2.5vw;
 `
 
 export const PotatoButton = styled.div`
+    /* Size */
     width: 40px;
     height: 40px;
+
+    /* Font */
     font-size: xx-large;
-    margin-right: 10px;
-    margin-left: 10px;
-    border-radius: 999px;
     color: ${Colors.White};
+
+    /* Appearance */
     background-color: #c4c4c4;
     box-shadow: 4px 4px 18px rgba(0, 0, 0, 0.08);
+    border-radius: 999px;
+
+    /* Layout */
+    margin-right: 10px;
+    margin-left: 10px;
+    display: grid;
+    place-items: center center;
 `
 
 // for clicking
 export const PotatoContainer = styled.div`
+    /* Layout */
     grid-area: splash;
 
+    /* Appearance */
     cursor: pointer;
 `
--- src/common/dashboard/sending_styles.tsx
+++ src/common/dashboard/sending_styles.tsx
@@ -4,13 +4,14 @@ import { Colors } from 'theme/Colors'
 // Used for PotatoInterface (sending mode)
 // ------------------------------------------------------------------------
 export const SendingModeContainer = styled.div`
-    display: grid;
+    /* Size */
     height: 100%;
     width: 100%;
 
+    /* Layout */
+    display: grid;
     grid-template-columns: 2fr 1fr 1fr 2fr;
     grid-template-rows: 20% 30% 10% 10% 10% 20%;
-
     grid-template-areas:
         '. . . .'
         'splash splash splash splash'
@@ -18,58 +19,71 @@ export const SendingModeContainer = styled.div`
         '. or or .'
         '. button1 button2 .'
         '. . . .';
-
     place-items: center center;
 `
 
 export const BigText = styled.div`
+    /* Layout */
+    grid-area: splash;
+
+    /* Font */
     font-size: 4vw;
     text-align: center;
-    grid-area: splash;
 `
 
 export const CopyContainer = styled.div`
-    grid-area: text;
-    display: grid;
+    /* Size */
     height: 100%;
 
+    /* Layout */
+    grid-area: text;
+    display: grid;
     grid-template-columns: 4fr 1fr;
     grid-template-rows: auto 1fr;
-
     grid-column-gap: 20px;
-
     grid-template-areas:
         'url button'
         'copied copied';
-
     place-items: center center;
 `
 
 export const CopyBox = styled.div`
-    display: grid;
+    /* Size */
     height: 100%;
     width: 100%;
     padding-right: 5px;
     padding-left: 5px;
-    place-items: center center;
+
+    /* Appearance */
     background-color: ${Colors.LightGray};
     border-radius: 999px;
+
+    /* Layout */
+    display: grid;
+    place-items: center center;
 `
 
 export const CopyMessage = styled.div`
+    /* Layout */
     grid-area: copied;
+
+    /* Appearance */
     color: green;
+
+    /* Font */
     font-weight: bold;
 `
 
 export const Or = styled.div`
+    /* Size */
     width: 50%;
 
+    /* Layout */
     display: grid;
     grid-area: or;
-
     grid-template-columns: 3fr 2fr 3fr;
     grid-template-rows: auto;
 
+    /* Font */
     font-weight: bold;
 `
