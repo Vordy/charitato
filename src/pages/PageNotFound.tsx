79de2c4a0dd59beb8b7fdfdc1cccf8c2bc49d43c
Daniel Scott <dasc0325@colorado.edu>
2019-12-09 13:56:50 -0700

    [minor fix] added theme to 404

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { localize } from 'assets/strings/localize'
+import { getTheme } from 'theme/themes'
 
 const Page = styled.div`
+    /* Colors */
+    background-color: ${getTheme().background};
+    color: ${getTheme().foreground};
+
     font-family: Helvetica Nueue, roboto, Arial, Helvetica, sans-serif;
     font-size: 4vw;
     width: 100%;
